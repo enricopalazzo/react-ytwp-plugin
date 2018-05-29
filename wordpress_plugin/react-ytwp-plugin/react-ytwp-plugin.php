e8b475f76740ff136493add9d9c7c3066241b2e5
enricopalazzo <federicodib@gmail.com>
2018-05-29 15:45:02 -0400

    fixing some css

--- wordpress_plugin/react-ytwp-plugin/react-ytwp-plugin.php
+++ wordpress_plugin/react-ytwp-plugin/react-ytwp-plugin.php
@@ -9,38 +9,34 @@ Author URI: federicodib@gmail.com
 License: 
 License URI: 
 */
-if (!function_exists("extra_post_info")) {
-    function extra_post_info($content)
-    {
-        $extra_info = "EXTRA INFOcc";
-        return $content . $extra_info;
-    }
-    add_filter('the_content', 'extra_post_info');
-}
-
 function los_scripts()
 {
     wp_register_script('scripts-dv', plugins_url('/assets/js/scripts.js', __FILE__));
-    wp_register_script('scripts-dv', plugins_url('/assets/js/main.fb853b6a.js', __FILE__));
+    wp_register_script('mainjs', plugins_url('/assets/js/main.165f1a88.js', __FILE__));
     wp_enqueue_style('bootstrap4', 'https://cdn.rawgit.com/twbs/bootstrap/48938155eb24b4ccdde09426066869504c6dab3c/dist/css/bootstrap.min.css');
+    wp_enqueue_style('slick', plugins_url('/assets/css/styles.css', __FILE__));
+
    
 }
 add_action('wp_enqueue_scripts', 'los_scripts');
 
-function insert_reactYT($atts)
+function insert_reactYT($atts,  $tag = '')
 {
+    $atts = array_change_key_case((array)$atts, CASE_LOWER);
+  
     $a = shortcode_atts(array(
-        'channelID' => '',
-        'API_KEY' => '',
-        'TERM' => ''
-    ), $atts);
+        'channelid' => '',
+        'api_key' => '',
+        'term' => ''
+    ), $atts, $tag);
 
     $pasarObject = array(
       'dirUrl' => plugin_dir_url( __FILE__ ),
       'from_shortcode' => $a 
     );
-    
+    wp_enqueue_script('scripts-dv');
     wp_localize_script( 'scripts-dv', 'from_php', $pasarObject);
+    wp_enqueue_script('mainjs');
 
     
     $html = <<<HTML
